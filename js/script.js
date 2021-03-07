e3fef291337171e1c6f688f50a6a6221c786ccd5
brittany <brinidoss@gmail.com>
2021-03-07 07:47:48 -0500

    final

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ for (let i =0; i<6; i++){
       happiness += 2;
       energy -= 1;
     } else if (action === "walk" && (energy<1)){
-      alert("Not enough energy for a walk.")
+      alert("Not enough energy to enjoy a walk.")
 }
 }
 console.log(name + " has " + happiness + " happiness and " + energy + " energy.")
