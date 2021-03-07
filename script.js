6b810b063f717cc2ba1dc541443ba2b08a46d95f
brittany <brinidoss@gmail.com>
2021-03-07 06:34:49 -0500

    finished cycle

---
+++ script.js
@@ -0,0 +1,19 @@
+let name = prompt("Name your pet.")
+var happiness = 0
+var energy = 0
+
+for (let i =0; i<6; i++){
+    let action = prompt("feed, pet, or walk?")
+
+    if(action === "feed"){
+      energy += 2;
+    } else if (action === "pet"){
+      happiness += 1;
+    } else if (action === "walk" && (energy>=1)){
+      happiness += 2;
+      energy -= 1;
+    } else if (action === "walk" && (energy<1)){
+      alert("Not enough energy for a walk.")
+}
+}
+console.log(name + " has " + happiness + " happiness and " + energy + " energy.")
